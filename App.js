178fa323166c1d9ead9b36e86866ef9b2bb27860
chechen-respublic <121053834+chechen-respublic@users.noreply.github.com>
2023-01-02 00:19:42 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+let socket = new WebSocket("ws://localhost:3000/");
+
+function send_msg(){
+
+        
+        socket.OPEN
+
+        socket.onopen = () => {
+          let h3_ws_status = document.querySelector('.h3_ws_status')
+          h3_ws_status.innerHTML = 'ONLINE'
+          socket.send('Привет мир')
+        }
+        socket.onclose = () => {
+          let h3_ws_status = document.querySelector('.h3_ws_status')
+          h3_ws_status.innerHTML = 'DISCONNECTED'
+          }
+
+
+        }
+        socket.onmessage = (msg) => {
+          let _msg = document.createElement('h4')
+          _msg.innerHTML = msg.data
+          let _chat = document.querySelector('._chat')
+          _chat.appendChild(_msg)
+          let status_send = document.querySelector('.status_send')
+          if(msg.data === 'Печатает...'){status_send.innerHTML = 'Печатает...'}else{status_send.innerHTML = ''}
+        }
+
+        
