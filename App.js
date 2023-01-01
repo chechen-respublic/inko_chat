c2d08c113ccf1489d6d3ed468510d6b673a18e7f
chechen-respublic <121053834+chechen-respublic@users.noreply.github.com>
2023-01-02 01:24:39 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-let socket = new WebSocket("ws://http://5cdb-188-0-188-195.ngrok.io");
+let socket = new WebSocket("ws://http://913e-188-0-188-195.ngrok.io");
+
+socket.onopen = () => {
+  let h3_ws_status = document.querySelector('.h3_ws_status')
+  h3_ws_status.innerHTML = 'ONLINE'
+  
+}
+
+socket.onclose = () => {
+  let h3_ws_status = document.querySelector('.h3_ws_status')
+  h3_ws_status.innerHTML = 'DISCONNECTED'
+  }
 
 function send_msg(){
-
+  
+      let msg_inp = document.querySelector('.msg_inp')
         
-        socket.OPEN
+      socket.send(msg_inp.value)    
+        
+      }
 
-        socket.onopen = () => {
-          let h3_ws_status = document.querySelector('.h3_ws_status')
-          h3_ws_status.innerHTML = 'ONLINE'
-          socket.send('Привет мир')
-        }
-        socket.onclose = () => {
-          let h3_ws_status = document.querySelector('.h3_ws_status')
-          h3_ws_status.innerHTML = 'DISCONNECTED'
-          }
-
-
-        }
         socket.onmessage = (msg) => {
           let _msg = document.createElement('h4')
           _msg.innerHTML = msg.data
@@ -24,6 +26,8 @@ function send_msg(){
           _chat.appendChild(_msg)
           let status_send = document.querySelector('.status_send')
           if(msg.data === 'Печатает...'){status_send.innerHTML = 'Печатает...'}else{status_send.innerHTML = ''}
-        }
+
+          socket.close
+}
 
         
